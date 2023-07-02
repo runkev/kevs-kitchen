46f6edf6df4b2df0d2fb6d29d0c54ecba686786e
kpetow <92036249+kpetow@users.noreply.github.com>
2023-07-02 16:41:42 -0500

    home page tiles - add drop shadow

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -17,19 +17,19 @@ const Homepage = () => {
 
         <div className="flex flex-wrap justify-center gap-6">
           <Link to="/recipes/1" className="hover:scale-105">
-            <div className="h-64 w-80 border-black border-4 rounded-xl overflow-hidden">
+            <div className="h-64 w-80 border-black border-4 rounded-xl overflow-hidden drop-shadow-2xl">
               <h1 className='text-center font-bold bg-yellow-200'>Chocolate Chip Blondies</h1>
               <img src={recipe1} alt="Recipe 1" className="w-full h-full object-cover" />
             </div>
           </Link>
           <Link to="/recipes/2" className="hover:scale-105">
-            <div className="h-64 w-80 border-black border-4 rounded-xl overflow-hidden">
+            <div className="h-64 w-80 border-black border-4 rounded-xl overflow-hidden drop-shadow-2xl">
               <h1 className='text-center font-bold bg-yellow-200'>Salted Caramel Brownies</h1>
               <img src={recipe2} alt="Recipe 2" className="w-full h-full object-cover" />
             </div>
           </Link>
           <Link to="/recipes/3" className="hover:scale-105">
-            <div className="h-64 w-80 border-black border-4 rounded-xl overflow-hidden">
+            <div className="h-64 w-80 border-black border-4 rounded-xl overflow-hidden drop-shadow-2xl">
               <h1 className='text-center font-bold bg-yellow-200'>Healthy(er) Banana Bread</h1>
               <img src={recipe3} alt="Recipe 3" className="w-full h-full object-cover" />
             </div>
